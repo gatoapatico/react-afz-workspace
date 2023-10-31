7fc0660142a1fccd677da703426f9757d8c2456d
ALVARO FERNANDEZ <aferzu@gmail.com>
2023-10-31 17:02:46 -0500

    Meme Generator Final - UseEffect

--- src/Meme_Generator/components/Main.jsx
+++ src/Meme_Generator/components/Main.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react';
-import memeImage from '../images/memeimg.png'
-import memeData from '../memeData'
+import { useEffect, useState } from 'react';
 
 export default function Main() {
 
@@ -10,28 +8,52 @@ export default function Main() {
         randomImage: "http://i.imgflip.com/1bij.jpg"
     });
 
-    const [allMemeImages, setAllMemeImages] = useState(memeData);
+    function handleChange(event) {
+        const {name, value} = event.target;
+        setMeme(prevMeme =>({
+            ...prevMeme,
+            [name]: value
+        }));
+    }
+
+    const [allMemes, setAllMemes] = useState([]);
     
     function getRandomMeme() {
-        const MAX_LENGTH = allMemeImages.data.memes.length;
+        const MAX_LENGTH = allMemes.length;
         const index = Math.floor(Math.random() * MAX_LENGTH);
         setMeme(prevMeme => ({
             ...prevMeme,
-            randomImage: allMemeImages.data.memes[index].url
+            randomImage: allMemes[index].url
         }));
     }
+
+    /* WITHOUT ASYNC FUNCTIONS WAY */
+    /* useEffect(() => {
+        fetch('https://api.imgflip.com/get_memes')
+            .then(res => res.json())
+            .then(data => setAllMemes(data.data.memes))
+    }, []); */
+
+    useEffect(() => {
+        async function getMemes() {
+            const res = await fetch('https://api.imgflip.com/get_memes');
+            const data = await res.json();
+            setAllMemes(data.data.memes);
+        }
+        getMemes();
+    }, []);
 
     return (
         <main className="main-container">
             <div className="form">
-                <input type="text" placeholder="Top Text"/>
-                <input type="text" placeholder="Bottom Text"/>
+                <input type="text" name="topText" value={meme.topText} placeholder="Top Text" onChange={handleChange}/>
+                <input type="text" name="bottomText" value={meme.bottomText} placeholder="Bottom Text" onChange={handleChange}/>
                 <button onClick={getRandomMeme}>Get a new meme image 🎭</button>
             </div>
             <div className='image-meme'>
                 <img src={meme.randomImage} alt="Meme Image" />
-                <h1 className='txt_1'>SHUT UP</h1>
-                <h1 className='txt_2'>AND TAKE MY MONEY</h1>
+                <h1 className='txt_1'>{meme.topText}</h1>
+                <h1 className='txt_2'>{meme.bottomText}</h1>
             </div>
         </main>
     )
